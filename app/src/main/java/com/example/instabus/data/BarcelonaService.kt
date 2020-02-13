4588f564126d029df26c5a4eb57e43a24c8fb2db
zioua <houssamziouany@gmail.com>
2020-02-13 10:57:20 +0100

    test

--- app/src/main/java/com/example/instabus/data/BarcelonaService.kt
+++ app/src/main/java/com/example/instabus/data/BarcelonaService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 interface BarcelonaService {
 
         @GET("/bus/nearstation/latlon/%2041.3985182/2.1917991/1.json/")
-         fun getBarcelonaData(): Call<com.example.a3andm.data.Response>
+         fun getBarcelonaData(): Call<com.example.instabus.data.Response>
 
 
 }
